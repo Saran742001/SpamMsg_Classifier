df402fdfb11f5bc6279cc1ebd67ebe9750af0197
sarankumar <sarankumar742001@gmail.com>
2026-02-09 12:24:00 +0530

    all files are removed from the spam classifier

--- spam_classifier/src/load_data.py
+++
@@ -1,8 +0,0 @@
-# src/load_data.py
-import pandas as pd
-
-def load_dataset(path="data/spam.csv"):
-    df = pd.read_csv(path, encoding="latin-1", on_bad_lines='skip')
-    df = df[['label','message']]
-    df.dropna(inplace=True)
-    return df
